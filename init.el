788290e58d9df68ea545328d2a886b6ac99d2fce
Yury Ponomarev <underside@ya.ru>
2020-10-07 00:30:46 +0300

    Which-key install

--- init.el
+++ init.el
@@ -534,20 +534,14 @@ tab-stop-list (quote (4 8))
   (yas-global-mode 1))
 
 ;;which-key
-;; (use-package which-key
-;;   :init
-;;   (setq which-key-sort-order #'which-key-prefix-then-key-order
-;;         which-key-sort-uppercase-first nil
-;;         which-key-add-column-padding 1
-;;         which-key-max-display-columns nil
-;;         which-key-min-display-lines 6
-;;         which-key-side-window-slot -10)
-;;   :config
-;;   ;; general improvements to which-key readability
-;;   (set-face-attribute 'which-key-local-map-description-face nil :weight 'bold)
-;;   (which-key-setup-side-window-bottom)
-;;   (setq-hook! 'which-key-init-buffer-hook line-spacing 3)
-;; )
+(use-package which-key
+  :ensure t
+  :init
+  :config
+  ;; general improvements to which-key readability
+  (set-face-attribute 'which-key-local-map-description-face nil :weight 'bold)
+  (which-key-setup-side-window-bottom)
+)
 
 ;;Ansible and Ansible-vault
 (use-package ansible
