9ea382c2fc1754193c1564854ce81d977995c656
Yury Ponomarev <underside@ya.ru>
2020-10-07 18:34:23 +0300

    Fix init.el config, add .spacemacs config

--- init.el
+++ init.el
@@ -1,7 +1,10 @@
 ;;Extra Repos and use-package installation
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-                         ("melpa"     . "https://melpa.org/packages/")
-                         ))
+(require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "https://melpa.org/packages/")
+   t)
 (package-initialize)
 
 ;; Bootstrap `use-package' 
@@ -166,7 +169,7 @@ tab-stop-list (quote (4 8))
 (size-indication-mode          t) ;; file size in persents
 
 
-;;Helm
+;; ;;Helm
 (use-package async
   :ensure t
   )
@@ -191,8 +194,7 @@ tab-stop-list (quote (4 8))
   (define-key evil-ex-map "f" 'helm-projectile-find-file)
 )
 
-
-;;
+;;yaml-mode
 (use-package yaml-mode
   :ensure t
   )
@@ -364,103 +366,6 @@ tab-stop-list (quote (4 8))
     (eval-after-load 'flycheck
       '(add-hook 'flycheck-mode-hook 'flycheck-yamllint-setup))))
 
-;;Neotree
-;; (use-package neotree
-;;   :ensure t
-;;   :bind
-;;   ("<f8>" . neotree-toggle)
-;;   :config
-;;   (setq neo-smart-open t)
-;; )
-
-;;Treemacs
-(use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-directory-name-transformer    #'identity
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-extension-regex          treemacs-last-period-regex-value
-          treemacs-file-follow-delay             0.2
-          treemacs-file-name-transformer         #'identity
-          treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   2
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-missing-project-action        'ask
-          treemacs-move-forward-on-expand        nil
-          treemacs-no-png-images                 nil
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        nil
-          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                      'left
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    nil
-          treemacs-recenter-after-tag-follow     nil
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   nil
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-asc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              1.5
-          treemacs-user-mode-line-format         nil
-          treemacs-user-header-line-format       nil
-          treemacs-width                         35
-          treemacs-workspace-switch-cleanup      nil)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("<f8>"   . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
-(use-package treemacs-evil
-  :after treemacs evil
-  :ensure t)
-
-(use-package treemacs-projectile
-  :after treemacs projectile
-  :ensure t)
-
-(use-package treemacs-icons-dired
-  :after treemacs dired
-  :ensure t
-  :config (treemacs-icons-dired-mode))
-
-(use-package treemacs-magit
-  :after treemacs magit
-  :ensure t)
 
 
 ;;Projectile
@@ -491,8 +396,7 @@ tab-stop-list (quote (4 8))
 (use-package magit
   :ensure t
   :config
-  (global-set-key (kbd "s-P") 'magit-status-with-prefix-arg)
-  (global-set-key (kbd "s-g") 'magit-status))
+  (global-set-key (kbd "<f4>") 'magit-status))
 
 
 (use-package evil-magit
