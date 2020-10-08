4f38b4dd33d02d792cc29f864162b8daccc0ac0c
Yury Ponomarev <underside@ya.ru>
2020-10-08 23:05:08 +0300

    Add doom-themes

--- init.el
+++ init.el
@@ -160,11 +160,11 @@ tab-stop-list (quote (4 8))
 (use-package bind-key)
 
 ;; Modeline settings
-(line-number-mode t)
-(column-number-mode t)
-(size-indication-mode t)
-(setq display-time-24hr-format t) ;; 24-hour
-(size-indication-mode          t) ;; file size in persents
+;; (line-number-mode t)
+;; (column-number-mode t)
+;; (size-indication-mode t)
+;; (setq display-time-24hr-format t) ;; 24-hour
+;; (size-indication-mode          t) ;; file size in persents
 
 
 ;; ;;Helm
@@ -197,18 +197,13 @@ tab-stop-list (quote (4 8))
   :ensure t
   )
 
-;; Themes
-;; Zenburn theme
-;; (use-package zenburn-theme
-;;   :ensure t
-;;   :load-path "themes"
-;;   :config
-;;   (load-theme 'zenburn 1)
-;;   )
-
 (use-package doom-themes
    :config
-   ;;(load-theme 'doom-wilmersdorf 1)
+   ;; (load-theme 'doom-one t)
+   ;; (load-theme 'doom-spacegrey t)
+   ;; (load-theme 'doom-zenburn t)
+   ;; (load-theme 'doom-nord t)
+   ;; (load-theme 'doom-wilmersdorf t)
    (load-theme 'doom-solarized-dark t)
    ;; Enable custom neotree theme (all-the-icons must be installed!)
    (doom-themes-neotree-config)
@@ -363,7 +358,7 @@ tab-stop-list (quote (4 8))
 (use-package flycheck
   ;; :init (global-flycheck-mode)
   :config
-  (global-flycheck-mode)
+  (global-flycheck-mode 1)
 )
 
 ;;Flycheck-yamllint
@@ -458,7 +453,7 @@ tab-stop-list (quote (4 8))
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" default)))
+    ("3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "4f01c1df1d203787560a67c1b295423174fd49934deb5e6789abd1e61dba9552" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "6c3b5f4391572c4176908bb30eddc1718344b8eaff50e162e36f271f6de015ca" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "5d09b4ad5649fea40249dd937eaaa8f8a229db1cec9a1a0ef0de3ccf63523014" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" default)))
  '(helm-completion-style (quote emacs))
  '(org-agenda-files (quote ("/mnt/org/notes/todo_personal.org")))
  '(package-selected-packages
@@ -471,6 +466,6 @@ tab-stop-list (quote (4 8))
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(all-the-icons-dsilver ((t (:foreground "white smoke")))))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
