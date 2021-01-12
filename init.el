0ecb3107c094fbc5b9cd5dbb696421b73a737e83
Yury Ponomarev <underside@ya.ru>
2021-01-12 11:23:48 +0300

    Helm opened in split frame not in existing buffers

--- init.el
+++ init.el
@@ -318,6 +318,10 @@ tab-stop-list (quote (4 8))
   :config
   (helm-mode 1)
   (define-key evil-ex-map "b " 'helm-mini)
+  ;; open Helm buffer in separate frame instead of using opened buffers
+  (setq helm-display-function 'helm-display-buffer-in-own-frame
+        helm-display-buffer-reuse-frame t
+        helm-use-undecorated-frame-option t)
 )
 
 (use-package helm-swoop
@@ -497,13 +501,29 @@ tab-stop-list (quote (4 8))
 )
 
 ;;Company-mode
+;; install shell-backend
+(use-package company-shell
+  :ensure t
+)
 (use-package company
   :ensure t
   :config
+  ;; add company backends
+  (add-to-list 'company-backends 'company-shell)
+  (add-to-list 'company-backends 'company-go)
+  (add-to-list 'company-backends 'company-python)
+
+  ;; company-hooks for different modes
   (add-hook 'after-init-hook 'global-company-mode)
   (add-hook 'go-mode-hook (lambda ()
-    (set (make-local-variable 'company-backends) '(company-go))
-    (company-mode)))
+                              (set (make-local-variable 
+                                    'company-backends) '(company-go)
+                                    )))
+  (add-hook 'shell-mode-hook
+            (lambda ()
+                (set (make-local-variable 'company-backends) '(company-shell))))
+
+  ;; base settings
   (setq company-tooltip-limit 20)
   (setq company-idle-delay 0.3) 
   (setq company-echo-delay 0.3)                          
@@ -755,21 +775,24 @@ tab-stop-list (quote (4 8))
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "4f01c1df1d203787560a67c1b295423174fd49934deb5e6789abd1e61dba9552" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "6c3b5f4391572c4176908bb30eddc1718344b8eaff50e162e36f271f6de015ca" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "5d09b4ad5649fea40249dd937eaaa8f8a229db1cec9a1a0ef0de3ccf63523014" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" default))
- '(ediff-split-window-function 'split-window-horizontally t)
- '(ediff-window-setup-function 'ediff-setup-windows-plain t)
- '(helm-completion-style 'emacs)
+   (quote
+    ("3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "4f01c1df1d203787560a67c1b295423174fd49934deb5e6789abd1e61dba9552" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "6c3b5f4391572c4176908bb30eddc1718344b8eaff50e162e36f271f6de015ca" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "5d09b4ad5649fea40249dd937eaaa8f8a229db1cec9a1a0ef0de3ccf63523014" "37144b437478e4c235824f0e94afa740ee2c7d16952e69ac3c5ed4352209eefb" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" default)))
+ '(ediff-split-window-function (quote split-window-horizontally) t)
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain) t)
+ '(helm-completion-style (quote emacs))
  '(ispell-dictionary-alist
-   '(("russian" "\\cy" "\\Cy" "[-]" nil
+   (quote
+    (("russian" "\\cy" "\\Cy" "[-]" nil
       ("-C" "-d" "ru-yeyo.multi" nil utf-8))
      ("english" "[a-zA-Z]" "[^a-zA-Z]" "[']" nil
       ("-d" "en_GB.multi" "--add-extra-dicts=en_GB-variant_1.multi" nil iso-8859-1))
      (nil "[A-Za-z]" "[^A-Za-z]" "[']" nil
-          ("-C" nil iso-8859-1))) t)
- '(ispell-extra-args '("--sug-mode=ultra" "--prefix=c:/mingw_mine"))
+          ("-C" nil iso-8859-1)))) t)
+ '(ispell-extra-args (quote ("--sug-mode=ultra" "--prefix=c:/mingw_mine")))
  '(ispell-program-name "aspell")
  '(package-selected-packages
-   '(helm-swoop helm-flycheck rainbow-delimiters diminish lsp-ivy lsp-ui deminish which-key dap-yaml dap-go dap-mode lsp-mode json-mode ob-go exec-path-from-shell multi-compile flymake-go flycheck-gometalinter treemacs-projectile treemacs-evil treemacs go-mode ob-http request restclient vterm htmlize beacon pomodoro org-pomodoro yasnippet-snippets dockerfile-mode jinja2-mode all-the-icons-ibuffer kubernetes-evil kubernetes adoc-mode helm-ag uniquify ansible ansible-vault jenkinsfile-mode eterm-256color evil-magit jdee groovy-mode popup-el emacs-async doom-modeline org-bullets yasnippet magit markdown-mode xterm-color flycheck-yamllint yaml-mode use-package helm flycheck evil-surround evil-matchit doom-themes company))
+   (quote
+    (helm-swoop helm-flycheck rainbow-delimiters diminish lsp-ivy lsp-ui deminish which-key dap-yaml dap-go dap-mode lsp-mode json-mode ob-go exec-path-from-shell multi-compile flymake-go flycheck-gometalinter treemacs-projectile treemacs-evil treemacs go-mode ob-http request restclient vterm htmlize beacon pomodoro org-pomodoro yasnippet-snippets dockerfile-mode  jinja2-mode all-the-icons-ibuffer kubernetes-evil kubernetes adoc-mode helm-ag uniquify ansible ansible-vault jenkinsfile-mode eterm-256color evil-magit jdee groovy-mode popup-el emacs-async doom-modeline org-bullets yasnippet magit markdown-mode xterm-color flycheck-yamllint yaml-mode use-package helm flycheck evil-surround evil-matchit doom-themes company)))
  '(projectile-mode t nil (projectile))
  '(recentf-mode t)
  '(temp-buffer-resize-mode t))
