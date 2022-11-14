5ae76e4726f7e81832cfa99aad3b8f5299e01457
Yuriy Ponomarev <yuriy.ponomarev@edna.ru>
2022-11-14 11:24:20 +0300

    Replace selectrum with Vertico

--- init.el
+++ init.el
@@ -396,29 +396,84 @@ shell exits, the buffer is killed."
 )
 
 ;;===Incremental narrowing
-;;selectrum (ivy and helm replacement)
-(use-package selectrum
+;; Enable vertico
+(use-package vertico
   :ensure t
-  :demand 
-  :config
-    (selectrum-mode +1)
+  :init
+  (vertico-mode)
 
-)
-;; nice sort approach for completion
+  ;; Different scroll margin
+  (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :ensure t
+  :init
+  (savehist-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t));; nice sort approach for completion
+
+
+;; Optionally use the `orderless' completion style.
 (use-package orderless
   :ensure t
-  :config
-    (setq orderless-skip-highlighting (lambda () selectrum-is-active))
-    (setq selectrum-highlight-candidates-function #'orderless-highlight-matches)
-    (setq completion-styles '(orderless))
-)
-;; consult - counsel and swiper replacement
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Use `consult-completion-in-region' if Vertico is enabled.
+;; Otherwise use the default `completion--in-region' function.
+(setq completion-in-region-function
+      (lambda (&rest args)
+        (apply (if vertico-mode
+                   #'consult-completion-in-region
+                 #'completion--in-region)
+               args)))
+
+
+
+;; consult: search, grep, project navigation 
 (use-package consult
   :ensure t
   :demand t
     )
 
-
+;; Undo replacement
 (use-package undo-fu
   :ensure t
     )
